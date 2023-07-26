54fad5b1d246b335b04d7c560f36d823305f40e6
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-07-26 23:19:24 +0700

    Add files via upload

--- get_suggestions.php
+++ get_suggestions.php
@@ -6,15 +6,35 @@ if (isset($_GET['q'])) {
   // Get the user input from the query parameter
   $input = $_GET['q'];
 
-  // Fetch suggestions from the database that start with the user input
-  $stmt = $db->prepare("SELECT DISTINCT tags FROM images WHERE tags LIKE :input");
-  $stmt->bindValue(':input', $input . '%', SQLITE3_TEXT);
+  // Separate the input by commas to get individual words
+  $words = explode(',', $input);
+
+  // Fetch all tags from the database
+  $stmt = $db->prepare("SELECT DISTINCT tags FROM images");
   $result = $stmt->execute();
 
-  // Store the suggestions in an array
-  $suggestions = array();
+  // Store all tags in a flat array
+  $allTags = array();
   while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $suggestions[] = $row['tags'];
+    $tags = explode(',', $row['tags']);
+    foreach ($tags as $tag) {
+      $suggestion = trim($tag);
+      if (!in_array($suggestion, $allTags)) {
+        $allTags[] = $suggestion;
+      }
+    }
+  }
+
+  // Filter the tags based on each word and store the suggestions
+  $suggestions = array();
+  foreach ($words as $word) {
+    $trimmedWord = trim($word);
+    foreach ($allTags as $tag) {
+      // Check if the tag starts with the word
+      if (stripos($tag, $trimmedWord) === 0) {
+        $suggestions[] = $tag;
+      }
+    }
   }
 
   // Send the suggestions as a JSON response
--- index.php
+++ index.php
@@ -44,10 +44,10 @@ $result = $stmt->execute();
       <h1 class="text-center fw-bold">ArtBOORU</h1>
       <p class="text-center"><small>simple image board</small></p>
       <div class="d-flex">
-        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search" onkeyup="showSuggestions(this.value)">
+        <input class="form-control me-2" type="search" name="search" placeholder="Search" aria-label="Search" onkeyup="debouncedShowSuggestions(this, 'suggestions1')" />
         <button class="btn btn-outline-success" type="submit">Search</button>
       </div>
-      <div class="dropdown" id="suggestions"></div>
+      <div class="dropdown" id="suggestions1"></div>
       <div class="mt-2 d-flex justify-content-center gap-2">
         <a class="fw-bold btn btn-sm" href="profile.php">profile</a>
         <a class="fw-bold btn btn-sm" href="setting.php">settings</a>
@@ -57,21 +57,34 @@ $result = $stmt->execute();
       </div>
     </form>
     <script>
-      function showSuggestions(input) {
+      var suggestedTags = {};
+
+      function debounce(func, wait) {
+        let timeout;
+        return function (...args) {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => {
+            func.apply(this, args);
+          }, wait);
+        };
+      }
+
+      function showSuggestions(input, suggestionsId) {
         // Get the suggestions element
-        var suggestionsElement = document.getElementById("suggestions");
+        var suggestionsElement = document.getElementById(suggestionsId);
 
         // Clear previous suggestions
         suggestionsElement.innerHTML = "";
 
         // If the input is empty, hide the suggestions
-        if (input === "") {
+        var inputValue = input.value.trim();
+        if (inputValue === "") {
           return;
         }
 
         // Fetch suggestions from the server using AJAX
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
+        xhttp.onreadystatechange = function () {
           if (this.readyState === 4 && this.status === 200) {
             var suggestions = JSON.parse(this.responseText);
 
@@ -79,27 +92,75 @@ $result = $stmt->execute();
             var dropdownDiv = document.createElement("div");
             dropdownDiv.classList.add("dropdown-menu", "show");
 
-            suggestions.forEach(function(suggestion) {
-              var a = document.createElement("a");
-              a.classList.add("dropdown-item");
-              a.textContent = suggestion;
-              a.onclick = function() {
-                // Set the clicked suggestion as the search input value
-                document.querySelector(".form-control").value = suggestion;
-                // Clear the suggestions
-                suggestionsElement.innerHTML = "";
-              };
-              dropdownDiv.appendChild(a);
+            // Clear the suggestedTags array before adding new suggestions
+            suggestedTags[suggestionsId] = [];
+
+            suggestions.forEach(function (suggestion) {
+              // Check if the suggestion is not already in the suggestedTags array
+              if (!suggestedTags[suggestionsId].includes(suggestion)) {
+                suggestedTags[suggestionsId].push(suggestion);
+
+                var a = document.createElement("a");
+                a.classList.add("dropdown-item");
+                a.textContent = suggestion;
+                a.onclick = function () {
+                  addTag(input, suggestionsId, suggestion);
+                };
+                dropdownDiv.appendChild(a);
+              }
             });
 
             // Append the dropdown to the suggestions element
             suggestionsElement.appendChild(dropdownDiv);
           }
         };
-        xhttp.open("GET", "get_suggestions.php?q=" + input, true);
+        xhttp.open("GET", "get_suggestions.php?q=" + inputValue, true);
         xhttp.send();
+      }
+
+      var debouncedShowSuggestions = debounce(showSuggestions, 300);
+  
+      function addTag(input, suggestionsId, tag) {
+        // Get the current input value
+        var currentValue = input.value.trim();
+
+        // If the current input value is empty, set the clicked suggestion as the input value
+        if (currentValue === "") {
+          input.value = tag;
+        } else {
+          // Otherwise, add the clicked suggestion as a new tag
+          var tags = currentValue.split(",").map(function (item) {
+            return item.trim();
+          });
+
+          // Check if the tag is not already in the tags list
+          if (!tags.includes(tag)) {
+            // Check if the tag starts with the current input prefix
+            var prefix = tags[tags.length - 1];
+            if (tag.toLowerCase().startsWith(prefix.toLowerCase())) {
+              // Remove the prefix from the new tag to avoid duplication
+              var newTag = tag.slice(prefix.length).trim();
+
+              // If there is a comma at the end of the prefix, remove it
+              if (tags[tags.length - 1].endsWith(",")) {
+                tags[tags.length - 1] = tags[tags.length - 1].slice(0, -1).trim();
+              }
+
+              // Add the new tag to the list without any whitespace
+              tags[tags.length - 1] = tags[tags.length - 1] + newTag;
+            } else {
+              tags.push(tag);
+            }
+
+            input.value = tags.join(", ");
+          }
         }
-      </script>
+
+        // Clear the suggestions
+        var suggestionsElement = document.getElementById(suggestionsId);
+        suggestionsElement.innerHTML = "";
+      }
+    </script>
     <?php include('bootstrapjs.php'); ?>
   </body>
 </html>
