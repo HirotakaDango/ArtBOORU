0499693f801196322d1752dd828c2c57e2117ed8
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2023-07-30 09:16:10 +0700

    Add files via upload

--- session_code.php
+++ session_code.php
@@ -5,7 +5,7 @@ session_start();
 $db = new SQLite3('database.sqlite');
 
 // Create the users table if it doesn't exist
-$db->exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, artist TEXT, pic TEXT, desc TEXT, bgpic TEXT)");
+$db->exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, artist TEXT)");
 
 // Check if the user is logging in or registering
 if (isset($_POST['login'])) {
@@ -40,34 +40,39 @@ if (isset($_POST['login'])) {
   $password = substr(htmlspecialchars($_POST['password']), 0, 40);
   $artist = substr(htmlspecialchars($_POST['artist']), 0, 40);
 
-  // Check if the username is already taken
-  $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
-  $stmt->bindValue(':username', $username, SQLITE3_TEXT);
-  $result = $stmt->execute();
-  $user = $result->fetchArray();
-  if ($user) {
-    echo "Username already taken.";
+  // Check if any of the fields are empty
+  if (empty($username) || empty($password) || empty($artist)) {
+    echo "Please fill in all the registration fields.";
   } else {
-    // Add the new user to the database
-    $stmt = $db->prepare("INSERT INTO users (username, password, artist) VALUES (:username, :password, :artist)");
+    // Check if the username is already taken
+    $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
     $stmt->bindValue(':username', $username, SQLITE3_TEXT);
-    $stmt->bindValue(':password', $password, SQLITE3_TEXT);
-    $stmt->bindValue(':artist', $artist, SQLITE3_TEXT);
-    $stmt->execute();
-    
-    // Generate a unique session ID and store it in a cookie
-    $session_id = uniqid();
-    setcookie('session_id', $session_id, time() + (7 * 24 * 60 * 60), '/');
-    
-    // Store the username in a cookie
-    setcookie('username', $username, time() + (7 * 24 * 60 * 60), '/');
-    
-    // Store the username in the session for future use
-    $_SESSION['username'] = $username;
-    
-    // Redirect the user to the homepage
-    header("Location: index.php");
-    exit;
+    $result = $stmt->execute();
+    $user = $result->fetchArray();
+    if ($user) {
+      echo "Username already taken.";
+    } else {
+      // Add the new user to the database
+      $stmt = $db->prepare("INSERT INTO users (username, password, artist) VALUES (:username, :password, :artist)");
+      $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+      $stmt->bindValue(':password', $password, SQLITE3_TEXT);
+      $stmt->bindValue(':artist', $artist, SQLITE3_TEXT);
+      $stmt->execute();
+
+      // Generate a unique session ID and store it in a cookie
+      $session_id = uniqid();
+      setcookie('session_id', $session_id, time() + (7 * 24 * 60 * 60), '/');
+
+      // Store the username in a cookie
+      setcookie('username', $username, time() + (7 * 24 * 60 * 60), '/');
+
+      // Store the username in the session for future use
+      $_SESSION['username'] = $username;
+
+      // Redirect the user to the homepage
+      header("Location: index.php");
+      exit;
+    }
   }
 } else {
   // Check if the session ID cookie exists and restore the session if it does
--- setpass.php
+++ setpass.php
@@ -1,98 +1,100 @@
 <?php
-//start session
+// start session
 session_start();
 
-//connect to sqlite database
+// connect to sqlite database
 $db = new SQLite3('database.sqlite');
 
-//check if user is logged in
-if(!isset($_SESSION['username'])){
-    header("Location: session.php");
-    exit();
+// check if user is logged in
+if (!isset($_SESSION['username'])) {
+  header("Location: session.php");
+  exit();
 }
 
-//check if form is submitted
-if(isset($_POST['submit'])){
-    //get input values and limit password length to 40 characters
-    $current_password = substr(htmlspecialchars($_POST['current_password']), 0, 40);
-    $new_password = substr(htmlspecialchars($_POST['new_password']), 0, 40);
-    $confirm_password = substr(htmlspecialchars($_POST['confirm_password']), 0, 40);
-    
-    //get username from session
-    $username = $_SESSION['username'];
-    
-    //get password from database
-    $stmt = $db->prepare('SELECT password FROM users WHERE username=:username');
-    $stmt->bindValue(':username', $username, SQLITE3_TEXT);
-    $result = $stmt->execute();
-    $row = $result->fetchArray(SQLITE3_ASSOC);
-    $password = $row['password'];
-    
-    //check if current password is correct
-    if($current_password == $password){
-        //check if new password and confirm password match
-        if($new_password == $confirm_password){
-            //update password in database
-            $stmt = $db->prepare('UPDATE users SET password=:new_password WHERE username=:username');
-            $stmt->bindValue(':new_password', $new_password, SQLITE3_TEXT);
-            $stmt->bindValue(':username', $username, SQLITE3_TEXT);
-            $stmt->execute();
-            
-            //redirect to profile page
-            header("Location: setting.php");
-            exit();
-        } else {
-            //display error message
-            $error = "New password and confirm password do not match";
-        }
+// check if form is submitted
+if (isset($_POST['submit'])) {
+  // get input values and limit password length to 40 characters
+  $current_password = substr(filter_var($_POST['current_password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW), 0, 40);
+  $new_password = substr(filter_var($_POST['new_password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW), 0, 40);
+  $confirm_password = substr(filter_var($_POST['confirm_password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_STRIP_LOW), 0, 40);
+
+  // get username from session
+  $username = $_SESSION['username'];
+
+  // get password from database
+  $stmt = $db->prepare('SELECT password FROM users WHERE username=:username');
+  $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+  $result = $stmt->execute();
+  $row = $result->fetchArray(SQLITE3_ASSOC);
+  $password = $row['password'];
+
+  // check if current password is correct
+  if ($current_password == $password) {
+    // check if new password and confirm password match
+    if ($new_password == $confirm_password) {
+      // update password in database
+      $stmt = $db->prepare('UPDATE users SET password=:new_password WHERE username=:username');
+      $stmt->bindValue(':new_password', $new_password, SQLITE3_TEXT);
+      $stmt->bindValue(':username', $username, SQLITE3_TEXT);
+      $stmt->execute();
+
+      // set success message
+      $_SESSION['success'] = "Password successfully changed";
+
+      // redirect to current page
+      header("Location: setpass.php");
+      exit();
     } else {
-        //display error message
-        $error = "Current password is incorrect";
+      // display error message
+      $error = "New password and confirm password do not match";
     }
+  } else {
+    // display error message
+    $error = "Current password is incorrect";
+  }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8"> 
+  <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Change Password</title>
-</head>
-<body>
+    <title><?php echo $artist; ?></title>
+    <link rel="icon" type="image/png" href="icon/favicon.png">
+    <?php include('bootstrapcss.php'); ?>
+  </head>
+  <body>
+    <?php include('header.php'); ?>
     <div class="container">
       <div class="text-center mt-4">
-        <h3 class="text-secondary fw-bold">Change Password</h3>
+        <h3 class="text-dark fw-bold"><i class="bi bi-key-fill"></i> Change Password</h3>
       </div>
-      <div class="container fw-bold text-secondary mt-4">
-        <?php if(isset($error)){ ?>
-            <p class="text-danger"><?php echo $error; ?></p>
-        <?php } ?>
+      <div class="container fw-bold text-dark mt-4">
+        <?php if (isset($_SESSION['success'])): ?>
+          <div class="alert alert-success"><?php echo $_SESSION['success']; ?></div>
+        <?php unset($_SESSION['success']); endif; ?>
+        <?php if (isset($error)): ?>
+          <div class="alert alert-danger"><?php echo $error; ?></div>
+        <?php endif; ?>
         <form method="POST">
-            <div class="mb-3">
-                <label class="form-label">Current Password:</label>
-                <input type="password" class="form-control" name="current_password" maxlength="40" pattern="^[a-zA-Z0-9_@.-]+$">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">New Password:</label>
-                <input type="password" class="form-control" name="new_password" maxlength="40" pattern="^[a-zA-Z0-9_@.-]+$">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Confirm Password:</label>
-                <input type="password" class="form-control" name="confirm_password" maxlength="40" pattern="^[a-zA-Z0-9_@.-]+$">
-            </div>
-            <div class="container">
-              <header class="d-flex justify-content-center py-3">
-                <ul class="nav nav-pills">
-                  <li class="nav-item"><button type="submit" class="btn btn-primary me-1 fw-bold" name="submit">Save</button></li>
-                  <li class="nav-item"><a href="setting.php" class="btn btn-danger ms-1 fw-bold">Back</a></li>
-                </ul>
-              </header>
-            </div>
+          <div class="form-floating mb-2">
+            <input type="password" class="form-control rounded-3 border text-dark fw-bold border-4" name="current_password" placeholder="Enter current password" maxlength="40" pattern="^[a-zA-Z0-9_@.-]+$">
+            <label for="floatingPassword" class="text-dark fw-bold">Enter current password</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="password" class="form-control rounded-3 border text-dark fw-bold border-4" name="new_password" max placeholder="Type new password"length="40" pattern="^[a-zA-Z0-9_@.-]+$">
+            <label for="floatingPassword" class="text-dark fw-bold">Type new password</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="password" class="form-control rounded-3 border text-dark fw-bold border-4" name="confirm_password" placeholder="Confirm new password" maxlength="40" pattern="^[a-zA-Z0-9_@.-]+$">
+            <label for="floatingPassword" class="text-dark fw-bold">Confirm new password</label>
+          </div>
+          <button type="submit" class="btn btn-primary w-100 fw-bold mb-2" name="submit">Save</button>
+          <a class="text-decoration-none fw-bold text-primary mb-2" href="setsupport.php">Having trouble?</a>
         </form>
       <div>
     </div>
-</body>
-</html>
+    <?php include('bootstrapjs.php'); ?>
+  </body>
+</html> 
